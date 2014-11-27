845ca4da1a9155b06b26309ec525fde08834076e
Dmitry Pyryeskin <dmitry@pyryeskin.com>
2014-11-27 15:17:14 -0500

    Added option to use URL instead of script's source

--- safari/main.js
+++ safari/main.js
@@ -77,7 +77,11 @@ function nextStep() {
 function renderHtmlAd() {
 	var form = document.forms.setup;
     prepareMraidView(form);
-    mraidview.setAdHeadScript(form.adHeadScript.value);
+    if (form.adHeadScriptIsURI.value) {
+    	mraidview.setAdHeadScript("document.write('<script src=" + form.adHeadScript.value + "></script>');");
+    } else {
+    	mraidview.setAdHeadScript(form.adHeadScript.value);
+    }
 	mraidview.setUseHtml(true, form.adFragment.value);
 	mraidview.render();
 	$('[href=#tabs-3]').click(); // switch to third tab
